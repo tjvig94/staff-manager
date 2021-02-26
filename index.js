5b8bd13d207948489d897ebe299914d1f5776f48
Tim Vigneau <tjvig94@gmail.com>
2021-02-26 14:12:26 -0500

    Added stylsheet to generated text

--- index.js
+++ index.js
@@ -239,6 +239,7 @@ const generatePage = () => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="./dist/style.css">
     <title>Team Viewer</title>
 </head>
 <body>
